ca8c2651528f8ab514efc9f3ea8cd9082bbd020c
Kseniya G Vo <vwk8888@gmail.com>
2019-03-26 16:02:24 +0200

    new

--- js/script.js
+++
@@ -1,7 +0,0 @@
-setInterval(function() {
-    $('.chicken_f2_static').toggleClass('off');
-    $('.chicken_f2_glow').toggleClass('off');
-    $('.chicken_f1_static').toggleClass('off');
-    $('.chicken_f1_glow').toggleClass('off');
-    
-  }, 2000);
